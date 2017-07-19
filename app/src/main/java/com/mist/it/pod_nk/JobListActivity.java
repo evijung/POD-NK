c2a1b5c4df4d2dd49cce6c258451d8c7e93224c7
evijung <evides01@gmail.com>
2017-07-19 10:49:06 +0700

    in pxd

--- app/src/main/java/com/mist/it/pod_nk/JobListActivity.java
+++ app/src/main/java/com/mist/it/pod_nk/JobListActivity.java
@@ -183,7 +183,7 @@ public class JobListActivity extends AppCompatActivity {
                 fullNameTextView.setText(driverNameString);
                 dateButton.setText(deliveryDateString);
 
-                JobListAdaptor jobListAdaptor = new JobListAdaptor(numberStrings, detailListStrings, arriveTimeStrings, context);
+                JobListAdaptor jobListAdaptor = new JobListAdaptor(numberStrings, detailListStrings, arriveTimeStrings, subJobNoStrings, context);
                 tripListView.setAdapter(jobListAdaptor);
 
                 for (int i = 0; i < subJobNoStrings.length; i++) {
@@ -197,15 +197,16 @@ public class JobListActivity extends AppCompatActivity {
     }
 
     protected class JobListAdaptor extends BaseAdapter {
-        String[] numberStrings;
+        String[] numberStrings, subJobNoStrings;
         String[][] detailListStrings, arriveTimeStrings;
         JobListViewHolder jobListViewHolder;
         Context context;
 
-        public JobListAdaptor(String[] numberStrings, String[][] detailListStrings, String[][] arriveTimeStrings, Context context) {
+        public JobListAdaptor(String[] numberStrings, String[][] detailListStrings, String[][] arriveTimeStrings, String[] subJobNoStrings, Context context) {
             this.numberStrings = numberStrings;
             this.detailListStrings = detailListStrings;
             this.arriveTimeStrings = arriveTimeStrings;
+            this.subJobNoStrings = subJobNoStrings;
             this.context = context;
         }
 
@@ -245,6 +246,7 @@ public class JobListActivity extends AppCompatActivity {
                     intent.putExtra("Login", loginStrings);
                     intent.putExtra("Date", dateString);
                     intent.putExtra("Position", position + 1);
+                    intent.putExtra("SubJobNo", subJobNoStrings[position]);
                     startActivity(intent);
                     Log.d("Tag", "Position ==> " + position);
 
--- app/src/main/java/com/mist/it/pod_nk/ManageJobActivity.java
+++ app/src/main/java/com/mist/it/pod_nk/ManageJobActivity.java
@@ -1,15 +1,31 @@
 package com.mist.it.pod_nk;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
+import static com.mist.it.pod_nk.MyConstant.urlGetJob;
 
 public class ManageJobActivity extends AppCompatActivity {
 
@@ -30,7 +46,7 @@ public class ManageJobActivity extends AppCompatActivity {
     @BindView(R.id.btnMJAStop)
     Button stopButton;
 
-    String dateString, tripNoString;
+    String dateString, tripNoString, subJobNoString;
     String[] loginStrings;
 
     @Override
@@ -41,7 +57,129 @@ public class ManageJobActivity extends AppCompatActivity {
 
         dateString = getIntent().getStringExtra("Date");
         tripNoString = getIntent().getStringExtra("Position");
-        loginStrings = getIntent().getStringArrayExtra("Login ");
+        loginStrings = getIntent().getStringArrayExtra("Login");
+        subJobNoString = getIntent().getStringExtra("SubJobNo");
+
+        jobNoTextView.setText("Trip " + tripNoString);
+
+
+        SyncGetJob syncGetJob = new SyncGetJob();
+        syncGetJob.execute();
+
+    }
+
+    class SyncGetJob extends AsyncTask<Void, Void, String> {
+        String[] subJobNoStrings, deliveryDateStrings, truckStrings, driverNameStrings, driverSirNameStrings, deliveryTripNoStrings, tripStartTimeStrings;
+        String[] tripStopTimeStrings, tripStartMileStrings, tripStopMileStrings;
+        String[][] detailListStrings, provinceStrings, arriveTimeStrings;
+        String[][][] jobNoStrings;
+        String[][][][] invoiceStrings, amountStrings;
+
+        @Override
+
+        protected String doInBackground(Void... params) {
+            try {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                RequestBody requestBody = new FormEncodingBuilder()
+                        .add("truck_id", loginStrings[0])
+                        .add("subjob_no", subJobNoString)
+                        .add("isAdd", "true")
+                        .build();
+                Request request = builder.post(requestBody).url(urlGetJob).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("Tag", s);
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+
+                JSONArray dataJsonArray = jsonObject.getJSONArray("data");
+                subJobNoStrings = new String[dataJsonArray.length()];
+                deliveryDateStrings = new String[dataJsonArray.length()];
+                truckStrings = new String[dataJsonArray.length()];
+                driverNameStrings = new String[dataJsonArray.length()];
+                driverSirNameStrings = new String[dataJsonArray.length()];
+                deliveryTripNoStrings = new String[dataJsonArray.length()];
+                tripStartMileStrings = new String[dataJsonArray.length()];
+                tripStartTimeStrings = new String[dataJsonArray.length()];
+                tripStopMileStrings = new String[dataJsonArray.length()];
+                tripStopTimeStrings = new String[dataJsonArray.length()];
+
+                detailListStrings = new String[dataJsonArray.length()][];
+                provinceStrings = new String[dataJsonArray.length()][];
+                arriveTimeStrings = new String[dataJsonArray.length()][];
+                jobNoStrings = new String[dataJsonArray.length()][][];
+                invoiceStrings = new String[dataJsonArray.length()][][][];
+                amountStrings = new String[dataJsonArray.length()][][][];
+
+                for (int i = 0;i < dataJsonArray.length();i++) {
+                    JSONObject jsonObject1 = dataJsonArray.getJSONObject(i);
+                    subJobNoStrings[i] = jsonObject1.getString("SubJobNo");
+                    deliveryDateStrings[i] = jsonObject1.getString("DeliveryDate");
+                    truckStrings[i] = jsonObject1.getString("Truck");
+                    driverNameStrings[i] = jsonObject1.getString("DriverName");
+                    driverSirNameStrings[i] = jsonObject1.getString("DriverSirname");
+                    deliveryTripNoStrings[i] = jsonObject1.getString("DeliveryTripNo");
+                    tripStartMileStrings[i] = jsonObject1.getString("TripStartMile");
+                    tripStartTimeStrings[i] = jsonObject1.getString("TripStartTime");
+                    tripStopMileStrings[i] = jsonObject1.getString("TripEndMile");
+                    tripStopTimeStrings[i] = jsonObject1.getString("TripEndTime");
+
+                    JSONArray delivPlaceJsonArray = jsonObject1.getJSONArray("DeliveryPlace");
+                    detailListStrings[i] = new String[delivPlaceJsonArray.length()];
+                    provinceStrings[i] = new String[delivPlaceJsonArray.length()];
+                    arriveTimeStrings[i] = new String[delivPlaceJsonArray.length()];
+
+                    jobNoStrings[i] = new String[delivPlaceJsonArray.length()][];
+                    invoiceStrings[i] = new String[delivPlaceJsonArray.length()][][];
+                    amountStrings[i] = new String[delivPlaceJsonArray.length()][][];
+
+                    for (int j = 0;j < delivPlaceJsonArray.length();j++) {
+                        JSONObject jsonObject2 = delivPlaceJsonArray.getJSONObject(i);
+                        detailListStrings[i][j] = jsonObject2.getString("DetailList");
+                        provinceStrings[i][j] = jsonObject2.getString("PROVINCE");
+                        arriveTimeStrings[i][j] = jsonObject2.getString("ArrivalTime");
+
+                        JSONArray jobNoJsonArray = jsonObject2.getJSONArray("JobNo");
+                        jobNoStrings[i][j] = new String[jobNoJsonArray.length()];
+
+                        invoiceStrings[i][j] = new String[jobNoJsonArray.length()][];
+                        amountStrings[i][j] = new String[jobNoJsonArray.length()][];
+
+                        for (int k = 0;k < jobNoJsonArray.length();k++) {
+                            JSONObject jsonObject3 = jobNoJsonArray.getJSONObject(i);
+                            jobNoStrings[i][j][k] = jsonObject3.getString("JobNo");
+
+                            JSONArray invoiceJsonArray = jsonObject3.getJSONArray("Invoice");
+                            invoiceStrings[i][j][k] = new String[invoiceJsonArray.length()];
+                            amountStrings[i][j][k] = new String[invoiceJsonArray.length()];
+
+                            for (int l = 0;l < invoiceJsonArray.length();l++) {
+                                JSONObject jsonObject4 = invoiceJsonArray.getJSONObject(i);
+                                invoiceStrings[i][j][k][l] = jsonObject4.getString("Invoice");
+                                amountStrings[i][j][k][l] = jsonObject4.getString("Amount");
+                            }
+                        }
+                    }
+                }
+
+
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
     @OnClick({R.id.btnMJAStart, R.id.btnMJAStop})
