77ee89033ade91e3fc2bde994741b2d28830e942
evijung <evides01@gmail.com>
2017-07-27 09:21:59 +0700

    Detail Job Activity pending

--- app/src/main/java/com/mist/it/pod_nk/DetailJobActivity.java
+++ app/src/main/java/com/mist/it/pod_nk/DetailJobActivity.java
@@ -1,10 +1,14 @@
 package com.mist.it.pod_nk;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -15,6 +19,10 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -49,8 +57,8 @@ public class DetailJobActivity extends AppCompatActivity {
     @BindView(R.id.btnDJAConfirm)
     Button confirmButton;
 
-    String dateString,subJobNoString, tripNoString, invoiceString;
-    String[] loginStrings;
+    String dateString, subJobNoString, tripNoString, storeString, storeIdString, arriveTimeString;
+    String[] loginStrings, invoiceStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +70,10 @@ public class DetailJobActivity extends AppCompatActivity {
         tripNoString = getIntent().getStringExtra("Position");
         loginStrings = getIntent().getStringArrayExtra("Login");
         subJobNoString = getIntent().getStringExtra("SubJobNo");
-        invoiceString = getIntent().getStringExtra("Place");
+        storeString = getIntent().getStringExtra("Place");
+
+        storeTextView.setText(getResources().getText(R.string.Store) + ": " + storeString);
+        dateTextView.setText(getResources().getText(R.string.Date) + ": " + dateString);
 
         SynGetJobDetail synGetJobDetail = new SynGetJobDetail();
         synGetJobDetail.execute();
@@ -75,12 +86,12 @@ public class DetailJobActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(Void... params) {
             try {
-                Log.d("TAG", "Send ==> " + subJobNoString + " , " + invoiceString);
+                Log.d("TAG", "Send ==> " + subJobNoString + " , " + storeString);
                 OkHttpClient okHttpClient = new OkHttpClient();
                 Request.Builder builder = new Request.Builder();
                 RequestBody requestBody = new FormEncodingBuilder()
                         .add("subjob_no", subJobNoString)
-                        .add("invoiceNo", invoiceString)
+                        .add("invoiceNo", storeString)
                         .add("isAdd", "true")
                         .build();
                 Request request = builder.post(requestBody).url(MyConstant.urlGetJobDetail).build();
@@ -98,8 +109,84 @@ public class DetailJobActivity extends AppCompatActivity {
             super.onPostExecute(s);
 
             Log.d("Tag", s);
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+
+                JSONArray jsonArray = jsonObject.getJSONArray("data");
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+
+                    storeIdString = jsonObject1.getString("StoreId");
+                    arriveTimeString = jsonObject1.getString("ArrivalTime");
+
+                    JSONArray jsonArray1 = jsonObject1.getJSONArray("Invoice");
+                    invoiceStrings = new String[jsonArray1.length()];
+
+                    for (int j = 0; j < jsonArray1.length(); j++) {
+                        JSONObject jsonObject2 = jsonArray.getJSONObject(j);
+                        invoiceStrings[j] = jsonObject2.getString("Invoice");
+                    }
+                }
+
+                arrivalTimeTextView.setText(getResources().getText(R.string.ArrivalTime) + ": " + arriveTimeString);
 
 
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    class InvoiceListAdaptor extends BaseAdapter {
+        Context context;
+        String[] invoiceStrings;
+        InvoiceListViewHolder invoiceListViewHolder;
+
+        public InvoiceListAdaptor(Context context, String[] invoiceStrings) {
+            this.context = context;
+            this.invoiceStrings = invoiceStrings;
+        }
+
+        @Override
+        public int getCount() {
+            return invoiceStrings.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(context).inflate(R.layout.invoice_listview, null);
+                invoiceListViewHolder = new InvoiceListViewHolder(convertView);
+                convertView.setTag(invoiceListViewHolder);
+            } else {
+                invoiceListViewHolder = (InvoiceListViewHolder) convertView.getTag();
+            }
+
+            invoiceListViewHolder.invoiceTextView.setText(invoiceStrings[position]);
+
+            return convertView;
+        }
+
+        class InvoiceListViewHolder {
+            @BindView(R.id.txtILInvoice)
+            TextView invoiceTextView;
+            @BindView(R.id.imgILCamera)
+            ImageView cameraImageView;
+
+            InvoiceListViewHolder(View view) {
+                ButterKnife.bind(this, view);
+            }
         }
     }
 
@@ -107,11 +194,11 @@ public class DetailJobActivity extends AppCompatActivity {
     class SynUpdateStatusArrive extends AsyncTask<Void, Void, String> {
         @Override
         protected String doInBackground(Void... params) {
-            try{
+            try {
                 OkHttpClient okHttpClient = new OkHttpClient();
                 Request.Builder builder = new Request.Builder();
                 RequestBody requestBody = new FormEncodingBuilder()
-                        .add("user_name", loginStrings[5] )
+                        .add("user_name", loginStrings[5])
                         .add("dealerName", "")
                         .add("subjob_no", "")
                         .add("invoiceNo", "")
